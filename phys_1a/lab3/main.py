40bd63beb599946c796562192f864a08f4899226
Allan Gongora <agongora4030@gmail.com>
2022-10-14 12:42:31 -0700

    i think i finished my graph for lab3

--- phys_1a/lab3/main.py
+++ phys_1a/lab3/main.py
@@ -2,31 +2,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+plt.style.use("fivethirtyeight")
 
-# def r2(function: ptype, x_data: pd.Series, y_data: pd.Series) -> float:
-#     rss = sum((y_data - function(x_data)) ** 2)
-#     tss = sum((y_data - y_data.mean()) ** 2)
-#     return 1 - (rss / tss)
+
+def r2(function, x_data, y_data) -> float:
+    rss = sum((y_data - function(x_data)) ** 2)
+    tss = sum((y_data - y_data.mean()) ** 2)
+    return 1 - (rss / tss)
 
 
 def gen_data() -> np.polynomial.polynomial.Polynomial:
     # setup
     disp = pd.read_csv("./data/data.csv")["distance(cm)"]
     df = pd.DataFrame({
-        "time (sec/60)": [i for i in range(len(disp))],
-        "displacement (cm)": disp,
+        "Time (s/60)": [i for i in range(len(disp))],
+        "x (cm)": disp,
     })
-
+    # time for velocity
+    df["Vtime (sec/60)"] = df["Time (s/60)"] + 0.5
     # velocity
-    df["velocity (cm/sec)"] = df["displacement (cm)"].diff() * 60
-    # avg velocity
-    avg_vel = []
-    for i in range(df.shape[0]):
-        try:
-            avg_vel.append((df["velocity (cm/sec)"][i] + df["velocity (cm/sec)"][i + 1])/2)
-        except:
-            avg_vel.append(np.NaN)
-    df["avg_vel (cm/sec)"] = avg_vel
+    df["Vave (cm/sec)"] = df["x (cm)"].diff() * 60
+    # Calc average accel
+    df["Aave (cm/sec^2)"] = df["Vave (cm/sec)"].diff()
     # save data
     df.to_csv("./data/created.csv", index=False)
     return
@@ -37,53 +34,47 @@ def graph():
     gen_data()
     # read in data
     df = pd.read_csv("./data/created.csv")
-
     # Changed for fig, ax
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx() # a copy of ax1
     # position time
-    ax1.scatter(df["time (sec/60)"], df["displacement (cm)"], label="position")
+    ax1.scatter(df["Time (s/60)"], df["x (cm)"], label="position", color="#008FD5")
     # average velocity time
-    ax1.scatter(df["time (sec/60)"], df["avg_vel (cm/sec)"], label="velocity")
+    ax1.scatter(df["Vtime (sec/60)"], df["Vave (cm/sec)"], label="velocity", color="#F96950")
+    # average accel time
+    ax2.scatter(df["Time (s/60)"], df["Aave (cm/sec^2)"] * 60, label="Aave", color="#E0DE36")
     # 5th order polynomial fit
     df.dropna(inplace=True) # dropping nans so fit doesn't fail
-    func = np.polynomial.Polynomial.fit(df["time (sec/60)"], df["velocity (cm/sec)"], 5)
+
+    # polynomial fit
+    func = np.polynomial.Polynomial.fit(df["Vtime (sec/60)"], df["Vave (cm/sec)"], 6)
     # accel func ae. velocity derivative
     derivative = func.deriv()
-    print(f"Function: {func}\nDerivative: {derivative}")
     # fit line
-    ax1.plot(df["time (sec/60)"], func(df["time (sec/60)"]), label=func)
-    # acceleration function
-    ax2.scatter(df["time (sec/60)"], derivative(df["time (sec/60)"]), label="acceleration", color="g") # * 60
+    ax1.plot(df["Time (s/60)"], func(df["Time (s/60)"]), label=f"Velocity fit: {func.convert()}", color="#F96950")
+    # acceleration function     
+    ax2.plot(df["Time (s/60)"], derivative(df["Time (s/60)"]) * 60, label=f"Acceleration: {derivative}", color="#0FA60F") # softer green
     # axis labels
-    ax1.set_xlabel("time (sec/60)")
+    ax1.set_xlabel("Time (s/60)")
     ax1.set_ylabel("position (cm)")
     ax2.set_ylabel("veloticy (cm/60 sec)")
-
-    # # position time
-    # plt.scatter(df["time (sec/60)"], df["displacement (cm)"])
-    # # avgerage velocity time
-    # plt.scatter(df["time (sec/60)"], df["avg_vel (cm/sec)"])
-    # # 5th order polynomial fit
-    # df.dropna(inplace=True) # dropping nans so fit doesn't fail
-    # func = np.polynomial.Polynomial.fit(df["time (sec/60)"], df["velocity (cm/sec)"], 5)
-    # # accel func ae. velocity derivative
-    # derivative = func.deriv()
-    # print(f"Function: {func}\nDerivative: {derivative}")
-    # # fit line
-    # plt.plot(df["time (sec/60)"], func(df["time (sec/60)"]))
-    # # acceleration function
-    # plt.scatter(df["time (sec/60)"], derivative(df["time (sec/60)"])) # * 60
-    # # show the graph
+    ax2.set_ylim(top=1000, bottom=-4000)
+    # show the graph
+    fig.legend(fontsize="small")
+    plt.text(50.3, 680, f"Velocity Fit R^2: {r2(func, df['Time (s/60)'], df['Vave (cm/sec)'])}", fontsize="small")
+    plt.subplots_adjust(left=.1, right=.9, top=.8)
     plt.show()
-    # coef_det = r2(func, df["time(60th of a second)"], df["avg_vel"])
     return
+
+
+def best_r2(x_data, y_data, max_deg, min_deg = 1) -> int:
+    scores = {}
+    for i in range(min_deg, max_deg + 1):
+        foo = np.polynomial.Polynomial.fit(x_data, y_data, i)
+        scores[i] = r2(foo, x_data, y_data)
+    print(scores)
+    return max(scores)
 
 
 if __name__ == "__main__":
     graph()
-# I think the graph i saw has different y axes, that's why accel looks diff.
-# accel is on it's on y, and vel/distance share an axis
-# my accel is fine just is in "the wrong spot" because it shares an axis on mine
-# which it shouldnt
-# https://stackoverflow.com/questions/14762181/adding-a-y-axis-label-to-secondary-y-axis-in-matplotlib
