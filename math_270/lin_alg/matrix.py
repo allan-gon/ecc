408b5fd97ca4de9fac36595e616591630043de02
allan <allan-gongora@lambdastudents.com>
2023-03-29 12:03:02 -0700

    fixed determinant

--- math_270/lin_alg/matrix.py
+++ math_270/lin_alg/matrix.py
@@ -298,73 +298,48 @@ class Matrix:
 
         for i in range(len(self)):
             # required because can have 0 in ref
-            scalar *= self[i][i]
+            scalar *= temp[i][i]
         with open("operations", "r") as file:
             contents = [line.split() for line in file.readlines()]
         for line in contents:
             if line[0] == "swap":
                 if line[1] != line[2]:
-                    print("There was a swap")
                     sign += 1
             elif line[0] == "divide":
                 scalar *= float(line[2])
-        return determinant * scalar * (-(1**sign))
+        return determinant * scalar * (((-1) ** sign))
 
 
 if __name__ == "__main__":
     # TODO: more thorough testing
-    # A = Matrix(
-    #     [
-    #         Vector([0, 1, 4, -1]),
-    #         Vector([1, 2, 3, 5]),
-    #         Vector([2, 5, 11, -3]),
-    #         Vector([3, 0, -2, 1]),
-    #     ]
-    # )
+    A = Matrix(
+        [
+            Vector([0, 1, 4, -1]),
+            Vector([1, 2, 3, 5]),
+            Vector([2, 5, 11, -3]),
+            Vector([3, 0, -2, 1]),
+        ]
+    )
 
-    # B = Matrix(
-    #     [
-    #         Vector([2, 3, 4, -3]),
-    #         Vector([2, 4, 1, 11]),
-    #         Vector([0, 1, -3, 2]),
-    #         Vector([6, 9, 12, 3]),
-    #     ]
-    # )
+    B = Matrix(
+        [
+            Vector([2, 3, 4, -3]),
+            Vector([2, 4, 1, 11]),
+            Vector([0, 1, -3, 2]),
+            Vector([6, 9, 12, 3]),
+        ]
+    )
 
-    # C = Matrix(
-    #     [
-    #         Vector([0, 0, 2, 0, 4]),
-    #         Vector([1, 2, 1, 0, 3]),
-    #         Vector([2, 4, 2, 0, 9]),
-    #         Vector([0, 0, 2, 3, 6]),
-    #         Vector([0, 2, 2, 2, 2]),
-    #     ]
-    # )
+    C = Matrix(
+        [
+            Vector([0, 0, 2, 0, 4]),
+            Vector([1, 2, 1, 0, 3]),
+            Vector([2, 4, 2, 0, 9]),
+            Vector([0, 0, 2, 3, 6]),
+            Vector([0, 2, 2, 2, 2]),
+        ]
+    )
 
     # print(A.det())
     # print(B.det())
-    # print(C.det())
-    from random import randint
-    from time import time
-
-    data = []
-    for _ in range(20):
-        temp = []
-        for _ in range(20):
-            temp.append(randint(-100, 100))
-        data.append(Vector(temp))
-    x = Matrix(data)
-    start = time()
-    temp = x.to_ref()
-    print(time() - start)
-    print(temp)
-
-    # A = Matrix(
-    #     [
-    #         Vector([2, 8, -2, 0]),
-    #         Vector([3, 13, 10, 5]),
-    #         Vector([-3, -12, 3, 2]),
-    #         Vector([0, 0, 4, 8]),
-    #     ]
-    # )
-    # print(A.det())
+    print(C.det())
